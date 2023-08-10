17cbb542fb9ac005f930d2fcd7bc86e56ec981cc
MatissMaleckis <82518754+MatissMaleckis@users.noreply.github.com>
2023-08-10 10:10:07 +0200

    Delete shell_script_clinker.sh

--- shell_script_clinker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source activate clinker
-clinker input/*.gb -o $dir_clinker/alignments.csv -dl "," -dc 4 -hl -ha -mo $dir_clinker/matrix.csv -p $dir_clinker/plot.html -s $dir_clinker/session.json -i 0.25 -f
