24aa5619868e367198946337ef368ce9196346bc
MatissMaleckis <82518754+MatissMaleckis@users.noreply.github.com>
2023-08-10 10:10:20 +0200

    Delete shell_script_cblaster.sh

--- shell_script_cblaster.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source activate cblaster
-cblaster search -qf $input.fasta -mh 3 -u 1 -mc 0 -m local -db custom_db/NCBI_P8A2_20230216/NCBI_P8A2_20230216.dmnd -b $input.csv -bde ',' -bkey max -bat identity -p $input.html
